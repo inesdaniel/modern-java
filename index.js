37923a932210b28e251fbf10fc074539007fcd99
Ines Daniel <i.danie8@gmail.com>
2018-08-16 20:26:10 -0500

    add math.js library

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ var moment = require('moment');
 console.log("in .js file");
 
 console.log(moment().subtract(10, 'days').calendar());
+
+
+// load math.js
+const math = require('mathjs');
+
+// use math.js
+console.log(math.sqrt(-4)); // 2i
